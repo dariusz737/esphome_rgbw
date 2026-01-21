e1e2c0e258d3fcfcebddd165cdb420de1628628e
Dariusz320 <81053092+dariusz737@users.noreply.github.com>
2026-01-22 00:06:36 +0100

    Update __init__.py

--- esphome/components/super_rgbw/__init__.py
+++ esphome/components/super_rgbw/__init__.py
@@ -1,16 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
+from esphome.components import output, number
 from esphome.const import CONF_ID
+
+super_rgbw_ns = cg.esphome_ns.namespace("super_rgbw")
+SuperRGBW = super_rgbw_ns.class_("SuperRGBW", cg.Component)
 
 CONF_OUT_R = "out_r"
 CONF_OUT_G = "out_g"
 CONF_OUT_B = "out_b"
 CONF_OUT_W = "out_w"
-CONF_FADE_TIME = "fade_time"
 
-super_rgbw_ns = cg.esphome_ns.namespace("super_rgbw")
-SuperRGBW = super_rgbw_ns.class_("SuperRGBW", cg.Component)
+CONF_R_NUMBER = "r_number"
+CONF_G_NUMBER = "g_number"
+CONF_B_NUMBER = "b_number"
+CONF_W_NUMBER = "w_number"
+CONF_DIM_NUMBER = "dim_number"
+CONF_FADE_TIME = "fade_time"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SuperRGBW),
@@ -20,8 +26,15 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_OUT_B): cv.use_id(output.FloatOutput),
     cv.Required(CONF_OUT_W): cv.use_id(output.FloatOutput),
 
-    cv.Optional(CONF_FADE_TIME, default="1s"): cv.positive_time_period_milliseconds,
+    cv.Required(CONF_R_NUMBER): cv.use_id(number.Number),
+    cv.Required(CONF_G_NUMBER): cv.use_id(number.Number),
+    cv.Required(CONF_B_NUMBER): cv.use_id(number.Number),
+    cv.Required(CONF_W_NUMBER): cv.use_id(number.Number),
+    cv.Required(CONF_DIM_NUMBER): cv.use_id(number.Number),
+
+    cv.Optional(CONF_FADE_TIME, default="1s"): cv.time_period,
 }).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -31,5 +44,11 @@ async def to_code(config):
     cg.add(var.set_out_g(await cg.get_variable(config[CONF_OUT_G])))
     cg.add(var.set_out_b(await cg.get_variable(config[CONF_OUT_B])))
     cg.add(var.set_out_w(await cg.get_variable(config[CONF_OUT_W])))
+
+    cg.add(var.set_r_number(await cg.get_variable(config[CONF_R_NUMBER])))
+    cg.add(var.set_g_number(await cg.get_variable(config[CONF_G_NUMBER])))
+    cg.add(var.set_b_number(await cg.get_variable(config[CONF_B_NUMBER])))
+    cg.add(var.set_w_number(await cg.get_variable(config[CONF_W_NUMBER])))
+    cg.add(var.set_dim_number(await cg.get_variable(config[CONF_DIM_NUMBER])))
 
     cg.add(var.set_fade_time(config[CONF_FADE_TIME].total_milliseconds))
